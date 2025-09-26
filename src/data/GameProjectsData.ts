7d91f811a9a7e3aa3638b906c2a0e388fd180462
srujanpant <srujanpant@gmail.com>
2025-09-27 03:55:16 +0530

    Portfolio updated

---
+++ src/data/GameProjectsData.ts
@@ -0,0 +1,134 @@
+import ProjectData from '@/data/ProjectData'
+
+export default [
+    new ProjectData("project-1", "Last Day Lockdown", "img/projects/LDL.gif", 
+    `
+    <div class="paragraph">
+     <strong>Last Day Lockdown</strong> is a narrative driven escape room puzzle game. It is your last day at the office and your colleagues have remodelled the entire office into
+                an escape room. Solve puzzles, relieve the office memories and escape!
+    </div>
+    <div class="paragraph center">
+        <iframe class="youtube" src="https://www.youtube.com/embed/kKbNK-KOCo8?si=xJR98BGYT7nCGLrD" frameborder="0" allowfullscreen></iframe>
+    </div>
+    <div class="paragraph center">
+        <a href="https://store.steampowered.com/app/2646700/Last_Day_Lockdown/" target="_blank"><img src="img/projects/SteamDemo.png" alt="Play Store badge" /></a>
+    </div>
+
+    <div class="paragraph">
+        <b>Game Designer and Lead Programmer</b>
+        <ul>
+        <li>Led a small team to complete development milestones and meet deadlines.</li>
+        <li>Conceptualized and implemented core gameplay mechanics, player controls, and interactive systems.</li>
+        <li>Designed immersive puzzles, escape-room scenarios, and multi-perspective narratives to enhance player engagement.</li>
+        <li>Developed levels and set pieces, integrating gameplay and narrative for cohesive experiences.</li>
+        <li>Presented the game on national and international platforms, demonstrating design and technical expertise.</li>
+        </ul>
+    </div>
+
+    <div class="paragraph center">
+        <img class="phone-screenshot" src="img/projects/LDL1.png" alt="Last Day Lockdown Screenshot" />
+        <img class="phone-screenshot" src="img/projects/LDL2.png" alt="Last Day Lockdown Screenshot" />
+        <img class="phone-screenshot" src="img/projects/LDL3.png" alt="Last Day Lockdown Screenshot" />
+        <img class="phone-screenshot" src="img/projects/LDL4.png" alt="Last Day Lockdown Screenshot" />
+
+    </div>
+    `, "#383838", true, true),
+
+    new ProjectData("project-2", "Azooma Escape", "img/projects/Azooma.gif", `
+    <div class="paragraph">
+        <strong>Azooma Escape</strong> is a stealthy social survival adventure. Step into the shoes of an introverted teenager trapped at your uncle's chaotic family celebration. Navigate crowded rooms, dodge nosy relatives, and outsmart relentless hosts in this hilarious stealth adventure. Outsmart your family, survive the festival, and escape before anyone notices!
+    </div>
+    <div class="paragraph center">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/Hlrx9G3x1Vg?si=bzdRpJrrCNyqk_M3" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
+
+    <div class="paragraph center">
+        <a href="https://store.steampowered.com/app/2896760/Azooma_Escape/" target="_blank"><img src="img/projects/Steam.png" alt="Play Store badge" /></a>
+    </div>
+
+    <div class="paragraph">
+        <b>Technical Sound Designer</b>
+        <ul>
+        <li>Designed and implemented an adaptive audio system that dynamically responds to player actions and in-game events.</li>
+        <li>Optimized sound assets and integrated FMOD middleware to deliver low-latency, high-quality audio across multiple platforms.</li>
+        <li>Collaborated with programmers and designers to synchronize sound effects with gameplay mechanics and interactive elements.</li>
+        <li>Developed custom tools and scripts to streamline sound design workflows.</li>
+        <li>Programmed FMOD parameter logic and RTPCs to drive dynamic audio responses based on player actions and game states.</li>
+        </ul>
+    </div>
+
+    <div class="paragraph center">
+    <img class="pc-screenshot" src="img/projects/AE1.jpg" alt="Azoom Escape Screenshot" />
+    <img class="pc-screenshot" src="img/projects/AE2.jpg" alt="Azoom Escape Screenshot" />
+    <img class="pc-screenshot" src="img/projects/AE3.jpg" alt="Azoom Escape Screenshot" />
+    <img class="pc-screenshot" src="img/projects/AE4.jpg" alt="Azoom Escape Screenshot" />
+    </div>
+    `, "#383838"),
+
+        new ProjectData("project-3", "Zenith Studio", "img/projects/Zenith.gif", `
+    <div class="paragraph">
+        <strong>At Zenith Studios</strong> I worked as a freelance game designer. Project details and assets remain confidential due to ongoing development and NDA requirements.
+    </div>
+
+    <div class="paragraph">
+        <b>Game Designer</b>
+        <ul>
+        <li>Created detailed game design documents outlining core mechanics, player progression and system interactions.</li>
+        <li>Designed narrative frameworks including character development, quest structures and multiplayer storyline integration.</li>
+        <li>Designed virtual economy systems including resource flows, pricing structures, and player progression incentives.</li>
+        <li>Established gameplay loops that maintain player retention while supporting monetization goals.</li>
+        <li>Collaborated with development teams to ensure design feasibility and technical implementation.</li>
+        </ul>
+    </div>
+    `, "#383838"),
+
+    new ProjectData("project-4", "Phantom Blaze", "img/projects/PhantomBlaze.gif", `
+    <div class="paragraph">
+        <strong>Phantom Blaze</strong> is a first person multiplayer tactical shooter. Hunt for powerful weapons, customize your loadout, and outsmart opponents in intense PvP and tactical team battles.
+    <div class="paragraph center">
+        <iframe class="youtube" src="https://www.youtube.com/embed/AiMQ9e00QWQ?si=qdWtO82Qg-ehvS_q" frameborder="0" allowfullscreen></iframe>
+    </div>
+
+    <div class="paragraph center">
+        <a href="https://play.google.com/store/apps/details?id=com.zonkstudio.phantomblaze&hl=en_IN" target="_blank"><img src="img/projects/play-store-logo.png" alt="Play Store badge" /></a>
+    </div>
+
+    <div class="paragraph">
+        <b>Senior Game Programmer</b>
+        <ul>
+        <li>Architected and implemented a scalable, layered UI system to support complex game features.</li>
+        <li>Designed and developed a level progression and XP system, enhancing player engagement.</li>
+        <li>Led API integration for cloud storage and connectivity, ensuring robust and secure data handling.</li>
+        <li>Directed the system design and implementation of a modular player and weapon customization framework.</li>
+        <li>Developed custom tools and plugins to optimize workflows and significantly improve development efficiency.</li>
+        </ul>
+    </div>
+
+    <div class="paragraph center">
+    <img class="pc-screenshot" src="img/projects/PB1.png" alt="Phantom Blaze Screenshot" />
+    <img class="pc-screenshot" src="img/projects/PB2.jpg" alt="Phantom Blaze Screenshot" />
+    <img class="pc-screenshot" src="img/projects/PB3.png" alt="Phantom Blaze Screenshot" />
+    </div>
+    `, "#383838", false, true),
+
+    new ProjectData("project-5", "One Energy Project", "img/projects/One_Energy.gif", `
+    <div class="paragraph">
+        <strong> Freelance gig </strong> for showcasing a car simulation that allowed users to visualize how a vehicle performed under different weather conditions, road types, and terrains.
+    </div>
+    <div class="paragraph">
+        <b>Unreal Engine Technical Consultant</b>
+        <ul>
+        <li>Provided technical support for a car simulation project, ensuring vehicle systems functioned correctly in Unreal Engine.</li>  
+        <li>Assisted with Blueprint implementation, tweaking, and optimization to handle diverse terrains, road types, and weather conditions.</li>
+        <li>Contributed to dashboard animations to visually showcase vehicle performance.</li>
+        </ul>
+    </div>
+
+
+    <div class="paragraph center">
+    <img class="pc-screenshot" src="img/projects/OneEnergy1.png" alt="One Energy Project Screenshot" />
+    <img class="pc-screenshot" src="img/projects/OneEnergy2.png" alt="One Energy Project Screenshot" />
+    <img class="pc-screenshot" src="img/projects/OneEnergy3.png" alt="One Energy Project Screenshot" />
+    </div>
+    `, "#383838")
+];
---
+++ src/views/About.vue
@@ -0,0 +1,47 @@
+<template>
+  <div style="margin-bottom: 80px;">
+    <h1>Hello!</h1>
+
+    <div class="paragraph">
+      <div>
+        I'm <strong>Srujan Pant</strong>, an indie game designer and developer.<br/>
+        I'm currently working on my game, Last Day Lockdown. I love designing puzzles, prototyping mechanics, and crafting narratives.
+      </div>
+
+      <div style="margin-top: 20px;">I've professionally worked on <router-link to="/game-projects">these projects</router-link> and personally worked on <router-link to="/other-projects">these</router-link> as well.</div>
+
+      <div style="margin-top: 40px;">I'm <strong>currently seeking opportunities</strong> as a game designer and developer. You can reach me at <a href="mailto:srujanpant@gmail.com">srujanpant@gmail.com</a> or <router-link to="/contact">through here</router-link>.</div>
+    </div>
+
+    <div class="photo">
+      <img src="img/avatar.png" alt="Avatar of Srujan" /> 
+    </div>
+
+    <div style="clear:both"></div>
+
+  </div>
+</template>
+
+<style scoped>
+.paragraph {
+  max-width: 700px;
+  margin-bottom: 20px;
+}
+
+.photo {
+  text-align: center;
+}
+
+@media only screen and (min-width: 620px){
+  .paragraph {
+    float: left;
+  }
+
+  .photo {
+    float: left;
+    padding: 10px;
+    padding-left: 80px;
+  }
+}
+
+</style>
